1285d0849119315387a761a2994a7b961d7fdb5b
JBTechDev <jbtechdev@gmail.com>
2019-11-22 10:23:52 -0800

    added comments for additional logic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs"); // To be able to access the file system
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
+inquirer // WHEN prompted for the developer's GitHub username and favorite color
     .prompt({
         message: "Enter a Github username",
         name: "username"
@@ -13,5 +13,27 @@ inquirer
             .get(queryUrl) // Send GET request to the 'queryUrl'
             .then(function(res) {
                 console.log(res.data); // The response object returned from the request should contain a `data` property which should be an array of the user's GitHub repos.
+                const repoNames = res.data.map(function(repo) {
+                    return repo.name;
+                });
+                console.log()
             });
     });
+
+    
+    // THEN a PDF profile is generated
+        // The PDF will be populated with the following:
+        // Profile image
+        // user name
+        // Links to the following:
+
+        // User location via Google Maps
+        // User GitHub profile
+        // User blog
+
+
+        // User bio
+        // Number of public repositories
+        // Number of followers
+        // Number of GitHub stars
+        // Number of users following
